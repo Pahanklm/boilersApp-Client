d19eb8a91520a64e1d340ff0142630629cf607c3
pavelklm <pavel.klm.verstka@gmail.com>
2023-08-15 23:57:30 +0300

    upgrade getLocation

--- app/api/geolocation.ts
+++ app/api/geolocation.ts
@@ -4,7 +4,12 @@ import api from '../axiosClient'
 
 
 export const getGeolocationFx = createEffect(async ({longitude , latitude}: IGeolocation) => {
-    const data = await api.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=${process.env.NEXT_PUBLIC_GEOAPI_KEY}`,{withCredentials: false})
+    const dataApprox = await api.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&lang=ru&apiKey=${process.env.NEXT_PUBLIC_GEOAPI_KEY}`,{withCredentials: false})
+
+    return dataApprox
+})
+export const getApproxGeolocationFx = createEffect(async () => {
+    const data = await api.get(`https://api.geoapify.com/v1/ipinfo?apiKey=${process.env.NEXT_PUBLIC_GEOAPI_KEY}`,{withCredentials: false})
 
     return data
 })
--- components/elements/CityButton/CityButton.tsx
+++ components/elements/CityButton/CityButton.tsx
@@ -3,9 +3,10 @@ import styles from "@/styles/cityButton/index.module.scss"
 import { useStore } from "effector-react"
 import { $mode } from "@/context/mode"
 import { $userCity, setUserCity } from "@/context/user"
-import { getGeolocationFx } from "@/app/api/geolocation"
+import { getApproxGeolocationFx, getGeolocationFx } from "@/app/api/geolocation"
 import { toast } from "react-toastify"
 import spinnerStyles from '@/styles/spinner/index.module.scss';
+import { useEffect } from "react"
 
 
 const CityButton = () => {
@@ -15,34 +16,51 @@ const CityButton = () => {
     const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
     const spinner = useStore(getGeolocationFx.pending)
 
-
-    const getCity = () => {
-        const options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0,
+    useEffect(() => {
+        const savedCity = localStorage.getItem('city');
+        if (!savedCity) {
+            getCity();
+            console.log(1);
+        } else {
+            setUserCity({ city: savedCity, street: '' });
         }
-        const success = async (pos: GeolocationPosition) => {
-            try {
-                const { latitude, longitude } = pos.coords
+    }, []);
 
-                const { data } = await getGeolocationFx({ latitude, longitude })
-
-                setUserCity({
-                    city: data.features[0].properties.city,
-                    street: data.features[0].properties.address_line1
-                })
-            } catch (error) {
-                toast.error((error as Error).message)
+    const getCity = async () => {
+        try {
+            const options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0,
             }
-        }
-        const error = (error: GeolocationPositionError) => {
-            toast.error(`${error.code} ${error.message} `)
-        }
+            const dataApprox = await getApproxGeolocationFx();
 
-        navigator.geolocation.getCurrentPosition(success, error, options)
+            let data;
+
+            try {
+                const pos = await new Promise<GeolocationPosition>((resolve, reject) => {
+                    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+                });
+
+                const { latitude, longitude } = pos.coords;
+
+                const result = await getGeolocationFx({ latitude, longitude });
+                data = result.data;
+            } catch (geoError) {
+
+            }
+
+            setUserCity({
+                city: (data && data.features[0]?.properties.city) || dataApprox.data.city.name,
+                street: (data && data.features[0]?.properties.address_line1) || dataApprox.data.city.name
+            });
+
+            // Сохраняем название города в localStorage
+            localStorage.setItem('city', (data && data.features[0]?.properties.city) || dataApprox.data.city.name);
+        } catch (error) {
+            toast.error((error as Error).message);
+        }
     }
-
     return (
         <button className={styles.city} onClick={getCity}
         > <span className={`${styles.city__span}  ${darkModeClass}`}>
