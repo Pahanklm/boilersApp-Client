8082a086fbc2105efd60d59863b17fb03d0014f8
pavelklm <pavel.klm.verstka@gmail.com>
2023-08-21 06:48:00 +0300

    fix bags

--- components/elements/CityButton/CityButton.tsx
+++ components/elements/CityButton/CityButton.tsx
@@ -58,7 +58,7 @@ const CityButton = () => {
     const registrationGeolocation = async () => {
         const data = await getRegistrationGeolocationFx()
         console.log(data);
-        sessionStorage.setItem('registrationLocation', data.data);
+        sessionStorage.setItem('registrationLocation', JSON.stringify(data.data));
         return data
     }
 
